f219825c211414a8d5a33c1006b4a798b0b0e9b3
monitoringdevice411@gmail.com <monitoringdevice411@gmail.com>
2021-04-11 00:32:14 +1000

    Random quote bot is working, YaY!

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,13 @@
 #Quote generator
+import random
 def kwote():
-    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    last = 13
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
 
 if __name__== "__main__":
   kwote()
